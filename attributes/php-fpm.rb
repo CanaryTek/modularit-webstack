40904e3ca4a2325dc22d2372b2e2e3eb74e324dd
Miguel Armas <kuko@canarytek.com>
2015-11-04 16:37:17 +0000

    fixes in attributes

--- attributes/php-fpm.rb
+++ attributes/php-fpm.rb
@@ -25,12 +25,12 @@ default['modularit-webstack']['php-fpm']['build_opts'] = "--disable-posix --with
 
 # Build dependencies
 case node['platform_family']
-when 'debian'
-  default['modularit-webstack']['php-fpm']['build_deps'] = %w[PLEASE_DEFINE]
-when 'rhel','fedora'
-  default['modularit-webstack']['php-fpm']['build_deps'] = %w[bzip2 gcc libxml2-devel postgresql-devel openssl-devel bzip2-devel libcurl-devel gd-devel libmcrypt-devel net-snmp-devel libssh2-devel]
-else
-  default['modularit-webstack']['php-fpm']['build_deps'] = %w[PLEASE_DEFINE]
+  when 'debian'
+    default['modularit-webstack']['php-fpm']['build_deps'] = %w[PLEASE_DEFINE]
+  when 'rhel','fedora'
+    default['modularit-webstack']['php-fpm']['build_deps'] = %w[bzip2 gcc libxml2-devel postgresql-devel openssl-devel bzip2-devel libcurl-devel gd-devel libmcrypt-devel net-snmp-devel libssh2-devel]
+  else
+    default['modularit-webstack']['php-fpm']['build_deps'] = %w[PLEASE_DEFINE]
 end
 # Additional pear packages
 default['modularit-webstack']['php-fpm']['pear_pkgs'] = %w[]
@@ -42,6 +42,4 @@ default['modularit-webstack']['php-fpm']['download_url'] = "http://es1.php.net/d
 default['modularit-webstack']['php-fpm']['php_versions'] = %w[5.4.45 5.5.30 5.6.14]
 # PHP install prefix
 default['modularit-webstack']['php-fpm']['install_prefix'] = "/opt/php"
-
-build_opts="--disable-posix --with-pgsql --with-pdo-pgsql --with-zlib --with-gd --with-openssl --with-curl --with-zlib --with-iconv --with-bz2 --with-gettext --with-fpm-user=nginx --with-fpm-group=nginx --enable-ftp --with-gettext --enable-fpm --with-mcrypt --enable-mbstring --enable-opcache --enable-bcmath --enable-calendar --enable-soap --enable-sockets --enable-zip --with-snmp"
 
